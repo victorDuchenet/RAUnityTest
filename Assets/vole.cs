1823d1fbe57ca14f848dcb7bb9ce634a43ee381c
victorDuchenet <victor.duchenet@ynov.com>
2017-11-08 16:07:52 +0100

    git init

---
+++ Assets/vole.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vole : MonoBehaviour
+{
+
+    // Use this for initialization
+    public float movementSpeed = 10;
+    public float rotateSpeed = 10;
+
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+        transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
+    }
+}
